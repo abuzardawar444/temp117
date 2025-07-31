4b20e4e5fde0021a3385cc0dcd2298fcfde422a8
abuzar <abuzardawar444@gmail.com>
2025-07-31 12:05:57 +0500

    refactor: remove database calls from fetch functions and add console logs for debugging

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,37 +5,13 @@ import db from "@/utils/db";
 import { redirect } from "next/navigation";
 
 export const fetchFeaturedProducts = async () => {
-  const products = await db.product.findMany({
-    where: {
-      // We get the products where the featured flag is set to true
-      featured: true,
-    },
-  });
-  return products;
+  console.log("hello from fetchFeaturedProducts");
 };
 
 export const fetchAllProducts = async ({ search = "" }: { search: string }) => {
-  return await db.product.findMany({
-    where: {
-      OR: [
-        { name: { contains: search, mode: "insensitive" } },
-        { company: { contains: search, mode: "insensitive" } },
-      ],
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+  console.log("hello from fetchAllProducts", search);
 };
 
 export const fetchSingleProduct = async (productId: string) => {
-  const product = await db.product.findUnique({
-    // We have two options, we have the entire product or it is going to be null
-    where: {
-      id: productId,
-    },
-  });
-  // If there is no matching product, then we want to redirect the user
-  if (!product) redirect("/products");
-  return product;
+  console.log("hello from fetchSingleProduct", productId);
 };
